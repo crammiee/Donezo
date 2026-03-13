3efadafe300ab46799c69a9bfb45e9c2b70bddff
crammiee <160859086+crammiee@users.noreply.github.com>
2026-03-13 22:57:24 +0800

    Implement socket.io integration for real-time communication

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 import 'dotenv/config';
 import http from 'http';
 import app from './app.js';
+import { initSocket } from './socket-service.js';
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
-
+initSocket(server);
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
