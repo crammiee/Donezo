dfdb1ec3cb06e55436368c6ae0162aa5f77e3f38
Andre Milan A. Arañas <aaaranas@up.edu.ph>
2026-03-17 18:51:57 +0800

    minor changes

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@ import 'dotenv/config';
 import http from 'http';
 import app from './app.js';
 import { initSocket } from './socket-service.js';
+import { startCronJobs } from './cron-service.js';
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 initSocket(server);
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-import { startCronJobs } from './cron-service.js';
-
-// After server.listen:
 startCronJobs();
